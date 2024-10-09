010b3c52a7275c10f5e652da6662a6b771bb50d7
jedi1118 <pobox.uno@gmail.com>
2024-10-09 15:09:04 -0700

    added mancala wikipedia link

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import Mancala from './Mancala.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+              <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://en.wikipedia.org/wiki/Mancala"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        What is Mancala?
         </a>
+
+      <header className="App-header">
         <Mancala />
       </header>
     </div>
