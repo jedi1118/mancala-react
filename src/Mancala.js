016cc4a312d3a04c70fd58147f07515d275aad23
jedi1118 <pobox.uno@gmail.com>
2024-10-17 12:09:01 -0700

    refactor state structure to be more flat, fix object deep copy with JSON.stringify and JSON.parse instead of Object.assign

--- src/Mancala.js
+++ src/Mancala.js
@@ -12,24 +12,38 @@ class Mancala extends React.Component {
         this.MAX_PITS = 6;
         this.PLAYER_1 = 0;
         this.PLAYER_2 = 1;
-        let player1Pits = [], player2Pits = [];
-        // generate "pits"
-        for (let i = 0; i < this.MAX_PITS; i++) {
-            player1Pits.push(this.getPitData(0, i, this.DEFAULT_SEEDS));
-            player2Pits.push(this.getPitData(1, i, this.DEFAULT_SEEDS));
-        }
-        // add "store" to the end
-        player1Pits.push(this.getPitData(0, this.MAX_PITS, 0, true));
-        player2Pits.push(this.getPitData(1, this.MAX_PITS, 0, true));
+        this.moves = [];// a list of moves - data[player1Pits, player2Pits], to be displayed one at a time with setInterval
+
+        // this holds keys to the pits, once initialized it should never change
+        this.playerPitList = {
+            [this.PLAYER_1]: [],
+            [this.PLAYER_2]: []
+        };// array, so we can access player data by player id
 
         this.state = {
             active: 0, // set active player
             message: '',
-            data: { 0: player1Pits, 1: player2Pits},// array, so we can access player data by player id
-            holding: 0,//{0: 0, 1: 0}// both side holds 0 seeds at the beginning
-            foo: player1Pits[0]
+            holding: 0// how many seeds in hand for the active/current player(the other player have no seeds in hand)
         };
-        this.moves = [];// a list of moves - data[player1Pits, player2Pits], to be displayed one at a time with setInterval
+        this.initPlayerData(this.PLAYER_1);
+        this.initPlayerData(this.PLAYER_2);
+    }
+    // initialize pit data and add to state
+    initPlayerData(playerId){
+        let i;
+        // create actual pit state data, ie { 'p0-0': {...}, 'p0-1': {...}, ...}
+        for (i = 0; i < this.MAX_PITS; i++) {
+            const pitData = this.getPitData(playerId, i, this.DEFAULT_SEEDS);
+            //this.setState({[pitData.key]: pitData});
+            this.state[pitData.key] = pitData;
+            // pit key
+            this.playerPitList[playerId].push(pitData.key);
+        }
+        // store
+        const storeData = this.getPitData(playerId, this.MAX_PITS, 0, true);
+        //this.setState({[storeData.key]: storeData});
+        this.state[storeData.key] = storeData;
+        this.playerPitList[playerId].push(storeData.key);
     }
     // generate pit data, we will consider "store" to be a special "pit",
     // this makes coding rule easier...?
@@ -40,6 +54,7 @@ class Mancala extends React.Component {
             player: player, 
             idx: idx,
             seeds: seeds,
+            key: `p${player}-${idx}`,
             isStore: isStore || false
         };
     }
@@ -50,15 +65,18 @@ class Mancala extends React.Component {
         }
         return otherSide;
     }
-    addMove(data, seedsInHand) {
-        // make new copy of move data
-        //let newHolding = seedsInHand;//{0: 0, 1: 0};
-    //    // let holdingCopy = Object.assign({}, this.state.holding);
-        //newHolding[playerSide] = seedsInHand;
-        let dataCopy = Object.assign({}, data);
-        console.log('addMove', dataCopy[this.state.active][0]);
-
-        this.moves.push({data: dataCopy, holding: seedsInHand/*newHolding*/});
+    addMove(pitKey, seedsInPit, seedsInHand) {
+        // create move data
+        // TODO: likely we will not need all these data, maybe 'seeds' should be enough
+        // pit data is not nested, use Object.assign to make shallow copy is safe here
+        let pitCopy = Object.assign({}, this.state[pitKey]);
+        pitCopy.seeds = seedsInPit;
+        this.moves.push({
+            key: pitKey,// to locate the pit
+            pitData: pitCopy, // data for the pit
+            holding: seedsInHand// seeds in hand
+        });
+        console.log('addMove', this.moves[this.moves.length-1]);
     }
     // removeMove() {
     //     const aMove = this.moves.shift();
@@ -66,94 +84,71 @@ class Mancala extends React.Component {
     //     this.setState({data: aMove});
     // }
     showMoves() {
-        console.log('showMoves', this.moves.length);
-        if (this.moves.length > 0) {
-            const aMove = this.moves.shift();
-            // display the more
-            console.log('show a move', aMove.data[0]);
-            this.setState({data: aMove.data, holding: aMove.holding});
-        }
-
-        // this.intervalRef = setInterval( ()=>{
-        //     if (this.moves.length > 0) {
-        //         const aMove = this.moves.shift();
-        //         // display the more
-        //         console.log('show a move', aMove.data[0][0]);
-        //         this.setState({});
-        //         this.setState({data: aMove.data, holding: aMove.holding});
-        //     }
-        //     if (this.moves.length === 0) {
-        //         console.log('showMoves: clear', this.moves.length);
-        //         clearInterval(this.intervalRef);
-        //         return;
-        //     }
-    
-        // }, 1000);
+        this.intervalRef = setInterval( ()=>{
+            if (this.moves.length > 0) {
+                const aMove = this.moves.shift();
+                // display the move
+                console.log('showMoves:', aMove.key, ' seeds:', aMove.pitData.seeds, ' holding:', aMove.holding);
+                this.setState(() => {
+                    return { [aMove.key]: aMove.pitData, holding: aMove.holding}
+                });
+            }
+            if (this.moves.length === 0) {
+                console.log('showMoves: clear', this.moves.length);
+                clearInterval(this.intervalRef);
+                return;
+            }
+        }, 1000);
     }
-    pitClicked = (data) => {
-        console.log('pitClicked', this, data);
-        if (data.player !== this.state.active) {// TODO: auto hide message
+    pitClicked = (pit) => {
+        console.log('pitClicked', this, pit);
+        if (pit.player !== this.state.active) {// TODO: auto hide message
             this.setState({ message: 'Click on a Pit on your side.'});
             return;
         }
         this.setState({ message: ''});
 
-        // make a copy of the player's pits
-        // let dataCopy = JSON.parse(JSON.stringify(this.state.data))
-        let dataCopy = Object.assign({}, this.state.data);
-        console.log('compare', dataCopy === this.state.data);
-        let playerPits = dataCopy[this.state.active];
-        let seedsInHand = playerPits[data.idx].seeds;// seeds in in-hand
-        playerPits[data.idx].seeds = 0;// pickup all - 0 out seeds in clicked pit
-        // set seeds in-hand
-        // let holding = seedsInHand0;//{0: 0, 1: 0};
-        // holding[this.state.active] = seedsInHand;
-        // add to the moves list
-        console.log('compare 2', dataCopy[this.state.active]);
-        this.setState({data: dataCopy});
-        this.setState({foo: dataCopy[0][0]});
-        // this.addMove(dataCopy, seedsInHand);
-        // this.showMoves();
-
-        let startIdx = data.idx+1;// starting pit to drop seed
-        // console.log('pitClicked: player=', this.state.active, ' clicked:', data.idx, ' startIdx=', startIdx);
-        // this.playTurn(dataCopy/*playerPits*/, seedsInHand, this.state.active, startIdx);
+        // set pit to 0, and seeds in-hand value
+        let seedsInHand = this.state[pit.key].seeds;// seeds in in-hand
+        this.addMove(pit.key, 0, seedsInHand);
+        // !!! NOTE: Object.assign({}, this.state) makes shallow copy, make changes on nested object will change value is state
+        // make a deep copy with: JSON.parse/JSON.stringify
+        let stateCopy = JSON.parse(JSON.stringify(this.state));
+        let startIdx = pit.idx+1;// starting pit to drop seed
+        console.log('pitClicked: player=', this.state.active, ' clicked:', pit.idx, ' startIdx=', startIdx);
+        this.playTurn(stateCopy, seedsInHand, this.state.active, startIdx);
     }
-    playTurn = (data, seedsInHand, playerSide, startIdx) => {
-        let playerPits = data[playerSide];
+    // NOTE: this create moves from a copy of the state data, so should not operate on this.state
+    playTurn = (stateCopy, seedsInHand, playerSide, startIdx) => {
+        let playerPitList = this.playerPitList[playerSide];
         console.log('@@@playTurn, side=', playerSide, ' startIdx:',startIdx, ' seedsInHand:', seedsInHand, ' moves:', this.moves.length);
         // try drop seed 1 by 1 into active player pits(including store)
         let idx = startIdx;
-        while (seedsInHand > 0 && idx < playerPits.length) {
+        while (seedsInHand > 0 && idx < playerPitList.length) {
             // drop seed if:
             // 1. not a store - any pit
             // 2. a store, and belong to active player
             // TODO: create steps for each pit change, then play it back with a delay
-
-            if (!playerPits[idx].isStore || (playerPits[idx].isStore && playerPits[idx].player === this.state.active)) {
-                playerPits[idx].seeds++;
+            let pit = stateCopy[playerPitList[idx]];
+            if (!pit.isStore || (pit.isStore && pit.player === this.stateCopy.active)) {
+                pit.seeds++;
                 seedsInHand--;
-            //     let newHolding = {0: 0, 1: 0};
-            //    // let holdingCopy = Object.assign({}, this.state.holding);
-            //    newHolding[playerSide] = seedsInHand;
-            //     let dataCopy = Object.assign({}, data);
-            //     this.moves.push({data: dataCopy, holding: newHolding});
-                // add to move data
-                this.addMove(data, seedsInHand);
-                console.log('-> place seed:', seedsInHand, ' pit:', playerPits[idx], ' moves:', this.moves.length);
+                // add to moves data
+                this.addMove(pit.key, pit.seeds, seedsInHand);
+                console.log('-> place seed:', seedsInHand, ' pit:', pit, ' moves:', this.moves[this.moves.length-1]);
             }
             // when seeds in-hand are empty, pickup all seeds from current pit again 
             // if (seedsInHand === 0) {
-            //     seedsInHand = playerPits[idx].seeds;
+            //     seedsInHand = playerPitList[idx].seeds;
             //     console.log('-> pickup seed:', seedsInHand);
             // }
             idx++;
         }
         // TODO: once finished turn, play back moves 1 by 1
         // create a copy of all pits
-        // let dataCopy = Object.assign({}, this.state.data);
-        // //let side = playerPits[0].player;
-        // dataCopy[side] = playerPits;
+        // let dataCopy = Object.assign({}, this.state.playerPitList);
+        // //let side = playerPitList[0].player;
+        // dataCopy[side] = playerPitList;
         // // update state with new pits data
         // this.setState({data: dataCopy});
      
@@ -163,7 +158,7 @@ class Mancala extends React.Component {
         //     if (otherSide > 1) {
         //         otherSide /= 2;
         //     }
-        //     let otherSidePits = Object.assign({}, this.state.data[otherSide]);
+        //     let otherSidePits = Object.assign({}, this.state.playerPitList[otherSide]);
         //     console.log('    switch side:', ' side:', otherSide, ' seedsInHand=', seedsInHand, );
         //     this.playTurn(seedsInHand, otherSidePits, 0);
         //}
@@ -176,33 +171,53 @@ class Mancala extends React.Component {
     // }
     render() {
         // generate UI components
-        let player1UI = [], player2UI = [];
-        for (let i = 0; i < this.MAX_PITS; i++) {
+        let player1UI = [], player2UI = [], i;
+        const p1pit = this.playerPitList[this.PLAYER_1];
+        const p2pit = this.playerPitList[this.PLAYER_2];
+        // console.log('render p1pit', p1pit, p2pit, this.state);
+
+        for (i = 0; i < this.MAX_PITS; i++) {
             // player 1
-            let p1pit = this.state.data[0][i];
-            player1UI.push(<Pit key={`${p1pit.player}.${p1pit.idx}`} config={p1pit} click={this.pitClicked}/>);
+            let p1key = p1pit[i];
+            player1UI.push(<Pit key={p1key} config={this.state[p1key]} click={this.pitClicked}/>);
+            // console.log('render player1UI', p1key, this.state[p1key]);
+
             // player 2
-            let p2pit = this.state.data[1][i];
-            player2UI.push(<Pit key={`${p2pit.player}.${p2pit.idx}`} config={p2pit} click={this.pitClicked}/>);
+            let p2key = p2pit[i];
+            player2UI.push(<Pit key={p2key} config={this.state[p2key]} click={this.pitClicked}/>);
+            // console.log('render player2UI', p2key, this.state[p2key]);
         }
         player2UI.reverse();
+        const store1Key = p1pit[this.MAX_PITS];
+        const store2Key = p2pit[this.MAX_PITS];
+
+        // console.log('render', store1Key, this.state[store1Key]);
         // pit order goes counter clockwise, need to reverse the other side
         return <div>
             <div id="mancala-game">
                 <div className="message player1">Message:{this.state.message}</div>
                 <div className="holding-area  player1">In-Hand:{this.PLAYER_2 === this.state.active ? this.state.holding: ''}</div>
                 <div>
-                    <Store config={this.state.data[this.PLAYER_2][this.MAX_PITS]}/>
+                    <Store config={this.state[store1Key]}/>
                     <div className="pit-container">
+                        {/* <ul>
+                            {
+                                p2pit.map((key) => (<Pit key={key} config={this.state[key]} click={this.pitClicked}/>))
+                            }
+                        </ul> */}
                         <ul>{player2UI}</ul>
                         <ul>{player1UI}</ul>
+                        {/* <ul>
+                            {
+                                p1pit.map((key) => (<Pit key={key} config={this.state[key]} id={key} click={this.pitClicked}/>))
+                            }
+                        </ul> */}
                     </div>
-                    <Store config={this.state.data[this.PLAYER_1][this.MAX_PITS]}/>
+                    <Store config={this.state[store2Key]}/>
                 </div>
                 <div className="holding-area player0">In-Hand:{this.PLAYER_1 === this.state.active ? this.state.holding: ''}</div>
                 <div className="message player0">Message:{this.state.message}</div>
             </div>
-            <div>{this.state.foo.seeds}</div>
         </div>
     }
 }
--- src/Pit.js
+++ src/Pit.js
@@ -4,26 +4,20 @@ import './pit.css';
 class Pit extends React.Component {
     constructor(props) {
         super(props);
-
+        // may not need this
         this.state = {
             config: props.config
         };
-        if (this.state.config.player === 0 && this.state.config.idx === 0) {
-            console.log('#######', props);
-        }
     }
     handlePitClick = () => {
         this.props.click(this.state.config);
     }
+    // componentDidUpdate(prevProps, prevState) {
+    //     console.log('###############', prevProps, prevState);
+    // }
     shouldComponentUpdate(prevProps, prevState) {
-        // if (this.state.config.player === 0 && this.state.config.idx === 0) {
-        //     console.log('shouldComponentUpdate:prevProps:', prevProps);
-        //     console.log("shouldComponentUpdate: state:", prevState.config, ' now:', this.state.config);
-        //     console.log("shouldComponentUpdate: seeds:", prevState.config.seeds, ' now:', this.state.config.seeds);
-        //     return true;
-        // }
-        if (prevState.config.seeds !== this.state.config.seeds) {
-            console.log('shouldComponentUpdate');
+        if (prevProps.config.seeds !== this.props.config.seeds) {
+            console.log('need update');
             return true;
         } else {
             return false;
@@ -31,8 +25,8 @@ class Pit extends React.Component {
     }
     render() {
         return <li 
-            className={`pit player${this.state.config.player}`}
-            ><div onClick={this.handlePitClick}>{this.state.config.seeds}</div></li>
+            className={`pit player${this.props.config.player}`}
+            ><div onClick={this.handlePitClick}>{this.props.config.seeds}</div></li>
     }
 }
 
