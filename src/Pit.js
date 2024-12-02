439ed68514039a4ae63970a1226651cd841beaad
jedi1118 <pobox.uno@gmail.com>
2024-12-02 11:17:51 -0800

    convert <Pit> from class component to functional component

--- src/Pit.js
+++ src/Pit.js
@@ -1,33 +1,26 @@
 import React from 'react';
 import './pit.css';
 
-class Pit extends React.Component {
-    constructor(props) {
-        super(props);
-        // may not need this
-        this.state = {
-            config: props.config
-        };
-    }
-    handlePitClick = () => {
-        this.props.click(this.state.config);
+function Pit({config, click}) {
+    function handlePitClick() {
+        click(config);
     }
     // componentDidUpdate(prevProps, prevState) {
     //     console.log('###############', prevProps, prevState);
     // }
-    shouldComponentUpdate(prevProps, prevState) {
-        if (prevProps.config.seeds !== this.props.config.seeds) {
-            console.log('need update');
-            return true;
-        } else {
-            return false;
-        }
-    }
-    render() {
-        return <li 
-            className={`pit player${this.props.config.player}`}
-            ><div id={this.props.config.key} onClick={this.handlePitClick}>{this.props.config.seeds}</div></li>
-    }
+    // shouldComponentUpdate(prevProps, prevState) {
+    //     if (prevProps.config.seeds !== this.props.config.seeds) {
+    //         console.log('need update');
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    return (
+        <li className={`pit player${config.player}`}>
+            <div id={config.key} onClick={handlePitClick}>{config.seeds}</div>
+        </li>
+    );
 }
 
 export default Pit;
